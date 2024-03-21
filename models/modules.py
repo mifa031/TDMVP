ba7fdd3478358fd4fa1299d85648c5980628c600
mifa031 <scifimov@naver.com>
2024-03-21 17:29:32 +0900

    Add files via upload

---
+++ models/modules.py
@@ -0,0 +1,30 @@
+from torch import nn
+
+class BasicConv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, transpose=False, act_norm=False):
+        super(BasicConv2d, self).__init__()
+        self.act_norm=act_norm
+        if not transpose:
+            self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding)
+        else:
+            self.conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding,output_padding=stride //2 )
+        self.norm = nn.GroupNorm(2, out_channels)
+        self.act = nn.LeakyReLU(0.2, inplace=True)
+
+    def forward(self, x):
+        y = self.conv(x)
+        if self.act_norm:
+            y = self.act(self.norm(y))
+        return y
+
+class ConvSC(nn.Module):
+    def __init__(self, C_in, C_out, stride, transpose=False, act_norm=True):
+        super(ConvSC, self).__init__()
+        if stride == 1:
+            transpose = False
+        self.conv = BasicConv2d(C_in, C_out, kernel_size=3, stride=stride,
+                                padding=1, transpose=transpose, act_norm=act_norm)
+
+    def forward(self, x):
+        y = self.conv(x)
+        return y
