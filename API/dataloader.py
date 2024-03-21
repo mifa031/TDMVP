b76d620611b9c71f1d6dd9ffa4211268ee408bbf
mifa031 <scifimov@naver.com>
2024-03-21 17:28:15 +0900

    Add files via upload

---
+++ API/dataloader.py
@@ -0,0 +1,6 @@
+from .dataloader_moving_mnist import load_data as load_mmnist
+
+def load_data(dataname,batch_size, val_batch_size, data_root, num_workers, **kwargs):
+    if dataname == 'mmnist':
+        return load_mmnist(batch_size, val_batch_size, data_root, num_workers)
+        
